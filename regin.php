6a4057fd23f9b36fb613f84d0be11cf89d993e92
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-03-26 21:11:19 +0500

    регистрация

--- regin.php
+++ regin.php
@@ -1,133 +1,107 @@
 <?php
-	session_start();
-	include("./settings/connect_datebase.php");
-	
-	if (isset($_SESSION['user'])) {
-		if($_SESSION['user'] != -1) {
-			
-			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
-			while($user_read = $user_query->fetch_row()) {
-				if($user_read[3] == 0) header("Location: user.php");
-				else if($user_read[3] == 1) header("Location: admin.php");
-			}
-		}
- 	}
+    include("./settings/jwt.php");
+    include("./settings/connect_datebase.php");
+    
+    // Если пользователь уже авторизован, отправляем его в кабинет
+    $user = get_user_from_jwt();
+    if ($user) {
+        if (isset($user['roll']) && $user['roll'] == 1) header("Location: admin.php");
+        else header("Location: user.php");
+        exit;
+    }
 ?>
+<!DOCTYPE HTML>
 <html>
-	<head> 
-		<meta charset="utf-8">
-		<title> Регистрация </title>
-		
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div class="top-menu">
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">БЗОПАСНОСТЬ  ВЕБ-ПРИЛОЖЕНИЙ</div>
-					Пермский авиационный техникум им. А. Д. Швецова
-				</a>
-			</div>
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class = "login">
-					<div class="name">Регистрация</div>
-				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Повторите пароль:</div>
-					<input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<a href="login.php">Вернуться</a>
-					<input type="button" class="button" value="Зайти" onclick="RegIn()" style="margin-top: 0px;"/>
-					<img src = "img/loading.gif" class="loading" style="margin-top: 0px;"/>
-				</div>
-				
-				<div class="footer">
-					© КГАПОУ "Авиатехникум", 2020
-					<a href=#>Конфиденциальность</a>
-					<a href=#>Условия</a>
-				</div>
-			</div>
-		</div>
-		
-		<script>
-			var loading = document.getElementsByClassName("loading")[0];
-			var button = document.getElementsByClassName("button")[0];
-			
-			function RegIn() {
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
-				
-				if(_login != "") {
-					if(_password != "") {
-						if(_password == _passwordCopy) {
-							loading.style.display = "block";
-							button.className = "button_diactive";
-							
-							var data = new FormData();
-							data.append("login", _login);
-							data.append("password", _password);
-							
-							// AJAX запрос
-							$.ajax({
-								url         : 'ajax/regin_user.php',
-								type        : 'POST', // важно!
-								data        : data,
-								cache       : false,
-								dataType    : 'html',
-								// отключаем обработку передаваемых данных, пусть передаются как есть
-								processData : false,
-								// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-								contentType : false, 
-								// функция успешного ответа сервера
-								success: function (_data) {
-									console.log("Авторизация прошла успешно, id: " +_data);
-									if(_data == -1) {
-										alert("Пользователь с таким логином существует.");
-										loading.style.display = "none";
-										button.className = "button";
-									} else {
-										location.reload();
-										loading.style.display = "none";
-										button.className = "button";
-									}
-								},
-								// функция ошибки
-								error: function( ){
-									console.log('Системная ошибка!');
-									loading.style.display = "none";
-									button.className = "button";
-								}
-							});
-						} else alert("Пароли не совподают.");
-					} else alert("Введите пароль.");
-				} else alert("Введите логин.");
-			}
-			
-			function PressToEnter(e) {
-				if (e.keyCode == 13) {
-					var _login = document.getElementsByName("_login")[0].value;
-					var _password = document.getElementsByName("_password")[0].value;
-					var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
-					
-					if(_password != "") {
-						if(_login != "") {
-							if(_passwordCopy != "") {
-								RegIn();
-							}
-						}
-					}
-				}
-			}
-			
-		</script>
-	</body>
+    <head> 
+        <meta charset="utf-8">
+        <title> Регистрация </title>
+        <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="top-menu">
+            <a href="#"><img src="img/logo1.png"/></a>
+            <div class="name">
+                <a href="index.php">
+                    <div class="subname">БЕЗОПАСНОСТЬ ВЕБ-ПРИЛОЖЕНИЙ</div>
+                    Пермский авиационный техникум им. А. Д. Швецова
+                </a>
+            </div>
+        </div>
+        <div class="space"> </div>
+        <div class="main">
+            <div class="content">
+                <div class="login">
+                    <div class="name">Регистрация</div>
+                
+                    <div class="sub-name">Логин:</div>
+                    <input name="_login" type="text" onkeypress="return PressToEnter(event)"/>
+                    <div class="sub-name">Пароль:</div>
+                    <input name="_password" type="password" onkeypress="return PressToEnter(event)"/>
+                    <div class="sub-name">Повторите пароль:</div>
+                    <input name="_passwordCopy" type="password" onkeypress="return PressToEnter(event)"/>
+                    
+                    <a href="login.php">Вернуться</a>
+                    <input type="button" class="button" value="Зарегистрироваться" onclick="RegIn()"/>
+                    <img src="img/loading.gif" class="loading" style="display:none;"/>
+                </div>
+                
+                <div class="footer">
+                    © КГАПОУ "Авиатехникум", 2020
+                </div>
+            </div>
+        </div>
+        
+        <script>
+            function RegIn() {
+                var loading = $(".loading");
+                var button = $(".button");
+                
+                var _login = $("[name='_login']").val();
+                var _password = $("[name='_password']").val();
+                var _passwordCopy = $("[name='_passwordCopy']").val();
+                
+                if(_login == "") { alert("Введите логин."); return; }
+                if(_password == "") { alert("Введите пароль."); return; }
+                if(_password != _passwordCopy) { alert("Пароли не совпадают."); return; }
+
+                loading.show();
+                button.attr('class', 'button_diactive');
+                
+                var data = new FormData();
+                data.append("login", _login);
+                data.append("password", _password);
+                
+                $.ajax({
+                    url         : 'ajax/regin_user.php',
+                    type        : 'POST',
+                    data        : data,
+                    processData : false,
+                    contentType : false, 
+                    success: function (_data) {
+                        // Убираем лишние пробелы из ответа
+                        var result = _data.trim(); 
+                        
+                        if(result == "-1") {
+                            alert("Пользователь с таким логином существует.");
+                            loading.hide();
+                            button.attr('class', 'button');
+                        } else {
+                            alert("Регистрация успешна! Теперь войдите в систему.");
+                            window.location.href = "login.php"; // После рега отправляем на вход
+                        }
+                    },
+                    error: function(){
+                        alert('Системная ошибка при регистрации!');
+                        loading.hide();
+                        button.attr('class', 'button');
+                    }
+                });
+            }
+            
+            function PressToEnter(e) {
+                if (e.keyCode == 13) RegIn();
+            }
+        </script>
+    </body>
 </html>
